a6313e44b9d6c07fba93dffa1c825dd573ed1b1b
Kat Reinhart <kat.reinhart@gmail.com>
2017-09-18 21:35:38 -0700

    Times Tables App is working!

--- Times Tables/ViewController.swift
+++ Times Tables/ViewController.swift
@@ -8,8 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var mySelection: Int = 0
+    
+    @IBOutlet var sliderValue: UISlider!
+    @IBOutlet var sliderValueOutput: UILabel!
+    
+    @IBOutlet var table: UITableView!
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+        print(sliderValue.value)
+        mySelection = Int(sliderValue.value)
+        sliderValueOutput.text = String(mySelection)
+        table.reloadData()
+        
+    }
+    
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        let cellContentString = String(indexPath.row) + " * " + String(mySelection) + " = "
+        
+        cell.textLabel?.text = cellContentString + String(mySelection * indexPath.row)
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
